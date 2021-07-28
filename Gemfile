4a63145802c21954029bfdb0ec95f2f9db5cfe9a
hyuntony <ankim90@gmail.com>
2021-07-28 15:59:24 -0400

    feature spec: product details

--- Gemfile
+++ Gemfile
@@ -45,9 +45,6 @@ group :development, :test do
   gem 'byebug'
   gem 'quiet_assets'
   gem 'dotenv-rails'
-end
-
-group :test do
   gem 'capybara'
   gem 'poltergeist'
   gem 'database_cleaner'
---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can see the product details when product partial is clicked" do
+    # ACT
+    visit root_path
+
+    # CLICK
+    page.first('.btn.btn-default.pull-right').click
+    
+    expect(page).to have_css 'article.product-detail', count: 1
+
+    # DEBUG / VERIFY
+    # save_screenshot
+  end
+
+end
